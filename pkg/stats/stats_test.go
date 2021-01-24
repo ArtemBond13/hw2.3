eb51ae6cd5829af07d2c0699dd1ecb002cc31788
Artem <garribond85@gmail.com>
2021-01-24 19:05:25 +0500

    add stats_test.go

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -1,1 +1,27 @@
 package stats
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortSlice(t *testing.T) {
+	type args struct {
+		transactions []int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int64
+	}{
+		// TODO: Add test cases.
+		{name: "slice", args: args{[]int64{120, 234, 12, 122, 234, 794}}, want: []int64{794, 234, 234, 122, 120, 12}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SortSlice(tt.args.transactions); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SortSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
